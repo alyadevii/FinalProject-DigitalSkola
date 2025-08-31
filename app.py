8729ba7f6c54a2d73f800017c1ad60c4ef26db08
alya devi anjani <alyadevianjani2@gmail.com>
2025-08-31 14:33:27 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from ml_app import run_ml_app
+def main():
+    menu = ['Home', 'Machine Learning']
+    choice = st.sidebar.selectbox('Menu', menu) 
+
+    if choice == 'Home':
+        st.title('Welcome to ' \
+        'Final Project Data Sapiens ' \
+        'Home Page')
+    elif choice == 'Machine Learning':
+        run_ml_app()
+        
+
+if __name__ == '__main__':
+    main()
