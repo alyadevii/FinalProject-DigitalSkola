3086e3f5615dfc6ea15e64511268fc84f7fe2ec2
alya devi anjani <alyadevianjani2@gmail.com>
2025-09-10 19:06:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ml_app import run_ml_app
 
 def main():
     st.set_page_config(
-        page_title="Final Project by Alya Devi Anjani",
+        page_title="Final Project by Data Sapiens",
         page_icon="💼",
         layout="wide"
     )
@@ -19,7 +19,7 @@ def main():
         st.markdown(
             """
             <h1 style='text-align: center; color: #4B8BBE;'>
-                Welcome to Prediction Employee Income Classification Final Project by Alya Devi Anjani
+                Welcome to Prediction Employee Income Classification Final Project by Data Sapiens
             </h1>
             """,
             unsafe_allow_html=True
@@ -36,7 +36,7 @@ def main():
         st.markdown(
             """
             <div style="max-width: 900px; margin: auto; font-size:18px; line-height:1.6; text-align: justify;">
-                Ini adalah website deploy by Alya Devi Anjani untuk mengklasifikasikan pendapatan seseorang ke dalam dua kategori, yaitu pendapatan kurang dari atau sama dengan 50 ribu dolar (<=50K) dan lebih dari 50 ribu dolar (>50K). Prediksi ini didasarkan pada berbagai atribut demografis dan pekerjaan seperti usia, jenis pekerjaan, tingkat pendidikan, status pernikahan, ras, jenis kelamin, jam kerja per minggu, serta informasi finansial seperti keuntungan atau kerugian modal. Dengan menggunakan data tersebut, model dapat membantu memperkirakan tingkat pendapatan seseorang berdasarkan karakteristik individu tersebut.
+                Ini adalah website deploy by Data Sapiens untuk mengklasifikasikan pendapatan seseorang ke dalam dua kategori, yaitu pendapatan kurang dari atau sama dengan 50 ribu dolar (<=50K) dan lebih dari 50 ribu dolar (>50K). Prediksi ini didasarkan pada berbagai atribut demografis dan pekerjaan seperti usia, jenis pekerjaan, tingkat pendidikan, status pernikahan, ras, jenis kelamin, jam kerja per minggu, serta informasi finansial seperti keuntungan atau kerugian modal. Dengan menggunakan data tersebut, model dapat membantu memperkirakan tingkat pendapatan seseorang berdasarkan karakteristik individu tersebut.
             </div>
             """,
             unsafe_allow_html=True
@@ -48,5 +48,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
